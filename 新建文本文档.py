35c2e6b01c989a65bb1f6fe9a05f224e64e57d78
sif5 <99023120+sif5@users.noreply.github.com>
2022-11-05 08:23:16 +0800

    Update 新建文本文档.py

--- 新建文本文档.py
+++ 新建文本文档.py
@@ -1,15 +1,16 @@
 #coding=utf-8
 import random
 import time
-import msvcrt
+#import msvcrt
 import decimal
-try:
-    times1=int(input("请输入想掷骰子的次数（0=一直骰）"))
-except:
-    print("这是数字？？默认为0")
-    times1=0
-    time.sleep(1)
-times=times1
+#try:
+#    times1=int(input("请输入想掷骰子的次数（0=一直骰）"))
+#except:
+#    print("这是数字？？默认为0")
+#    times1=0
+#    time.sleep(1)
+times1=0
+times=0
 shape=0
 zheng=0
 fan=0
@@ -24,8 +25,8 @@ if times > 0:
         times=times-1
         true1=true1+1
         print("正面:"+str(zheng)+",反面:"+str(fan)+",总次数:"+str(true1)+"频数:"+str(decimal.Decimal(zheng)/ decimal.Decimal(true1)))
-    print("请按任意键退出~")
-    ord(msvcrt.getch())
+#    print("请按任意键退出~")
+#    ord(msvcrt.getch())
 else:
     while True:
         shape=random.randint(0,1)
